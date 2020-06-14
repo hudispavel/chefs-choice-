b7b3cfc0d1da094150d761ffb6e37758542cf456
hudispavel <66915349+hudispavel@users.noreply.github.com>
2020-06-14 12:34:01 -0400

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,5 @@
+function submitModal() {
+    console.log("submit");
+    document.getElementById("mailing-list").style.display = "none";
+    document.getElementById("hooray-alert").style.display = "block";
+}
